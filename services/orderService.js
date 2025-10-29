bb62b999ef6ef85f747d1add6d4238c6420ffcd1
mohamed ibrahim <mohamed123ebrahim@gmail.com>
2025-10-29 18:11:34 +0300

    add webhook

--- services/orderService.js
+++ services/orderService.js
@@ -170,7 +170,7 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 const createOrderCheckout = async (session) => {
     const cartId = session.client_reference_id;
     const shippingAddress = session.metadata;
-    const orderPrice = session.display_items[0].amount / 100;
+    const orderPrice = session.amount_total / 100;
 
     const card = await Cart.findById(cartId);
     const user = await User.findOne({ email: session.customer_email });
